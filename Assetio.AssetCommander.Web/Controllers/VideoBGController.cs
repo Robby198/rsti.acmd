61054eb67407fe8e448295c70490073417c76f02
prumantir <perry.rumantir@gmail.com>
2017-03-09 20:11:51 +0700

    Added VideoBG (bahan untuk Landing Page)

---
+++ Assetio.AssetCommander.Web/Controllers/VideoBGController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assetio.AssetCommander.Web.Controllers
+{
+    public class VideoBGController : Controller
+    {
+        // GET: VideoBG
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
